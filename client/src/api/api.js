e23f95a77592df386a07b18493dda83a9d320de2
Pranay <pranaymaheshwari83@gmail.com>
2026-06-08 18:55:39 +0530

    Fix production API URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://fittrack-mukf.onrender.com';
-
+const API_URL = 'https://fittrack-mukf.onrender.com/api';
 // Create axios instance
 const api = axios.create({
   baseURL: API_URL,
